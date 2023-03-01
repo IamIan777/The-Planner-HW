78a9700dd3184e51199b46e474be4fd4b8798398
ian heap <ianheap77@gmail.com>
2023-02-28 22:59:23 -0500

    giving to github so I can get links

---
+++ Assets/script.js
@@ -0,0 +1,110 @@
+$(document).ready(function() {
+
+  $(".saveBtn").on("click", function() {
+    
+    var input = $(this).siblings(".description").val();
+    console.log(input)
+    var hour = $(this).parent().attr("id");
+    console.log(hour);
+
+    localStorage.setItem(hour, input);
+  });
+
+  var today = dayjs();
+  $("#currentDay").text(today.format("[Today is] dddd, MMM D"));
+
+  var moment = dayjs().hour()-10;
+
+  $("#to-do9").val(localStorage.getItem("hour9"));
+  $("#to-do10").val(localStorage.getItem("hour10"));
+  $("#to-do11").val(localStorage.getItem("hour11"));
+  $("#to-do12").val(localStorage.getItem("hour12"));
+  $("#to-do13").val(localStorage.getItem("hour13"));
+  $("#to-do14").val(localStorage.getItem("hour14"));
+  $("#to-do15").val(localStorage.getItem("hour15"));
+  $("#to-do16").val(localStorage.getItem("hour16"));
+  $("#to-do17").val(localStorage.getItem("hour17"));
+
+
+  if ((moment) > 9) {
+    $(hour9).css("background-color", "gray");
+  } else if ((moment) < 9) {
+    $(hour9).css("background-color", "green");
+  } else if ((moment) = 9) {
+    $(hour9).css("background-color", "red");
+  } 
+
+
+  if ((moment) > 10) {
+    $(hour10).css("background-color", "gray");
+  } else if ((moment) < 10) {
+    $(hour10).css("background-color", "green");
+  } else if ((moment) = 10) {
+    $(hour10).css("background-color", "red");
+  }
+
+
+  if ((moment) > 11) {
+    $(hour11).css("background-color", "gray");
+  } else if ((moment) < 11) {
+    $(hour11).css("background-color", "green");
+  } else if ((moment) = 11) {
+    $(hour11).css("background-color", "red");
+  }
+
+
+  if ((moment) > 12) {
+    $(hour12).css("background-color", "gray");
+  } else if ((moment) < 12) {
+    $(hour12).css("background-color", "green");
+  } else if ((moment) = 12) {
+    $(hour12).css("background-color", "red");
+  } 
+
+
+  if ((moment) > 13) {
+    $(hour13).css("background-color", "gray");
+  } else if ((moment) < 13) {
+    $(hour13).css("background-color", "green");
+  } else if ((moment) = 13) {
+    $(hour13).css("background-color", "red");
+  } 
+
+
+  if ((moment) > 14) {
+    $(hour14).css("background-color", "gray");
+  } else if ((moment) < 14) {
+    $(hour14).css("background-color", "green");
+  } else if ((moment) = 14) {
+    $(hour14).css("background-color", "red");
+  } 
+
+
+  if ((moment) > 15) {
+    $(hour15).css("background-color", "gray");
+  } else if ((moment) < 15) {
+    $(hour15).css("background-color", "green");
+  } else if ((moment) = 15) {
+    $(hour15).css("background-color", "red");
+  }
+
+
+  if ((moment) > 16) {
+    $(hour16).css("background-color", "gray");
+  } else if ((moment) < 16) {
+    $(hour16).css("background-color", "green");
+  } else if ((moment) = 16) {
+    $(hour16).css("background-color", "red");
+  }
+
+
+  if ((moment) > 17) {
+    $(hour17).css("background-color", "gray");
+  } else if ((moment) < 17) {
+    $(hour17).css("background-color", "green");
+  } else if ((moment) = 17) {
+    $(hour17).css("background-color", "red");
+  }
+
+
+})
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# The-Planner-HW
+# Description
+
+Day planner with updated date and time using moment.js. Color coded time marker to keep track of the day. Enter in appointments and to-dos, save and edit when needed.
+
+## Utilization
+
+1.HTML
+2.CSS
+3.JS
+4.Chrome for webpage
+5.Github for changes
+
+### links
+
+web application
+
